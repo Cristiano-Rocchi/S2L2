cfa051571ce4cadaa0eaaa85009c02064a38d650
Cristiano-Rocchi <168540556+Cristiano-Rocchi@users.noreply.github.com>
2024-05-07 17:37:53 +0200

    Update8-
    esercizi da 1 a 14

--- D2.js
+++ D2.js
@@ -232,10 +232,11 @@ console.log(me); */
   Scrivi del codice per creare un array inizialmente vuoto. Riempilo successivamente con i numeri da 1 a 10.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/* const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(array); */
 
 /* ESERCIZIO 15
-  Scrivi del codice per sostituire l'ultimo elemento dell'array, ovvero il valore 10, con il valore 100.
+  /* Scrivi del codice per sostituire l'ultimo elemento dell'array, ovvero il valore 10, con il valore 100.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
